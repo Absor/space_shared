04de711691a19c35f512fd193d3c960c311df669
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-06-03 22:04:12 +0300

    rotation update

--- lib/components/rotation_component.dart
+++ lib/components/rotation_component.dart
@@ -1,9 +1,26 @@
 part of space_shared;
 
 class RotationComponent extends Component {
-  num angle;
+  
+  num _angleInDegrees;
+  num _angleInRadians;
+  
+  num get angleInDegrees => _angleInDegrees;
+  num get angleInRadians => _angleInRadians;
+  
+  set angleInDegrees(num angle) {
+    _angleInDegrees = angle;
+    _angleInRadians = angle * _degreeRadian;
+  }
+  
+  set angleInRadians(num angle) {
+    _angleInRadians = angle;
+    _angleInDegrees = angle / _degreeRadian;
+  }
+  
+  static num _degreeRadian = PI / 180;
   
   Map toJson() {
-    return {"a":angle};
+    return {"a":angleInDegrees};
   }
 }
--- lib/systems/input_processing_system.dart
+++ lib/systems/input_processing_system.dart
@@ -1,16 +1,13 @@
 part of space_shared;
 
 class InputProcessingSystem implements System {
-  
-  num _degreeToRadian;
-  
+    
   bool enabled;
   int priority;
   
   List<Entity> _inputEntities;
   
   InputProcessingSystem() {
-    _degreeToRadian = PI / 180;
     _inputEntities = new List<Entity>();
   }
   
@@ -18,10 +15,10 @@ class InputProcessingSystem implements System {
     for (Entity entity in _inputEntities) {
       InputComponent input = entity.getComponent(InputComponent);
       RotationComponent rotation = entity.getComponent(RotationComponent);
-      rotation.angle += 80 * _degreeToRadian * input.turn * timeDelta / 1000;
+      rotation.angleInDegrees += 80 * input.turn * timeDelta / 1000;
       AccelerationComponent acceleration = entity.getComponent(AccelerationComponent);
-      acceleration.x = -sin(rotation.angle) * input.thrust * 100;
-      acceleration.y = cos(rotation.angle) * input.thrust * 100;
+      acceleration.x = -sin(rotation.angleInRadians) * input.thrust * 100;
+      acceleration.y = cos(rotation.angleInRadians) * input.thrust * 100;
     }
   }
   
